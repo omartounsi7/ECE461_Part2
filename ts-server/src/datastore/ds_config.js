d69fa67703ef690405f28826625b338852a3dc1a
kevintl1998 <kevintl1998@gmail.com>
2023-03-27 21:48:20 -0400

    added descriptions for each rest API endpoint

---
+++ ts-server/src/datastore/ds_config.js
@@ -0,0 +1,15 @@
+"use strict";
+// stores variables used by functions that modify our datastore
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.USER_KIND = exports.MODULE_KIND = exports.NAMESPACE = exports.datastore = void 0;
+const { Datastore } = require("@google-cloud/datastore");
+require('dotenv').config();
+// const projectId = process.env.PROJECT_ID;
+const datastore = new Datastore();
+exports.datastore = datastore;
+const NAMESPACE = "ece461";
+exports.NAMESPACE = NAMESPACE;
+const MODULE_KIND = "modules";
+exports.MODULE_KIND = MODULE_KIND;
+const USER_KIND = "users";
+exports.USER_KIND = USER_KIND;
---
+++ ts-server/src/datastore/ds_config.ts
@@ -0,0 +1,19 @@
+// stores variables used by functions that modify our datastore
+
+const {Datastore} = require("@google-cloud/datastore");
+
+require('dotenv').config();
+
+// const projectId = process.env.PROJECT_ID;
+
+
+const datastore = new Datastore();
+
+
+const NAMESPACE = "ece461";
+
+const MODULE_KIND = "modules";
+const USER_KIND = "users";
+
+
+export { datastore, NAMESPACE, MODULE_KIND, USER_KIND };
