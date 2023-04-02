a02848b5618b6d4e33b92d64c4ec6eb539243b58
kevintl1998 <kevintl1998@gmail.com>
2023-04-02 18:44:48 -0400

    deleted unnecessary prints

--- ts-server/src/datastore/modules.ts
+++ ts-server/src/datastore/modules.ts
@@ -222,8 +222,6 @@ async function downloadRepo(id: number): Promise<string> {
     console.log("Unimplemented function 'downloadRepo' from './src/datastore/modules.ts' was called.");
     const key = getKey(NAMESPACE, MODULE_KIND, id);
     const [entity] = await datastore.get(key);
-    console.log(typeof(entity));
-    console.log(entity);
     return entity;
 }
 
