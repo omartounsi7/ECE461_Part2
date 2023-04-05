48ae0e3a2be7a5f82af52a5bb5c943503dec0548
Max <mmanzhos@purdue.edu>
2023-04-05 19:11:35 -0400

    delete endpoint updated

--- ts-server/src/index.ts
+++ ts-server/src/index.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from 'express';
-import { filter } from 'lodash';
 import path from 'path';
-
+import * as ffi from 'ffi-napi';
 import {
     addRepo,
     updateRepo,
@@ -12,11 +11,15 @@ import {
     getAllReposPagenated,
     getAllRepos,
     updateRepoPackageAction,
+    downloadRepo,
     createRepoData
 } from "./datastore/modules";
 import { addUser } from "./datastore/users";
-import {deleteEntity, doesIdExistInKind, resetKind} from "/Users/maxim/Downloads/ECE461_Part2-main/ts-server/src/datastore";
-import {datastore, MODULE_KIND, NAMESPACE} from "/Users/maxim/Downloads/ECE461_Part2-main/ts-server/src/datastore/ds_config";
+import {datastore, MODULE_KIND, NAMESPACE} from "./datastore/ds_config";
+import { getKey, deleteEntity, resetKind, doesIdExistInKind } from "./datastore/datastore";
+
+const fs = require('fs');
+const { execFile } = require('child_process');
 
 /* * * * * * * * * * *
  * global variables  *
@@ -27,7 +30,6 @@ const HTML_PATH = ASSETS_PATH + "/html";
 const app = express();
 const port = 8080;
 
-
 app.use(express.json());
 
 
@@ -35,7 +37,6 @@ app.use(express.json());
  * Rest API endpoints  *
  * * * * * * * * * * * */
 
-// Fetch directory of packages
 app.post('/packages', async (req, res) => {
     res.send("packages endpoint");
 
@@ -86,8 +87,8 @@ app.post('/packages', async (req, res) => {
         if (offset === undefined) {
             offset = "0";
         }
-        // console.log(`offset: ${offset}`);
-        // console.log(queries);
+        console.log(`offset: ${offset}`);
+        console.log(queries);
 
         // do db actions
     }
@@ -97,20 +98,14 @@ app.post('/packages', async (req, res) => {
 
 });
 
-// Reset to default state
 app.delete('/reset', async (req, res) => {
-    console.log("reset endpoint");
+    res.send("reset endpoint");
 
     // get auth from header
     // look into https://jwt.io/
     //  let auth = req.header["X-Authorization"];
-    if(!req.headers.authorization){
-        res.sendStatus(400);
-    }
 
     // return 200 when registry is reset
-    await resetKind(MODULE_KIND);
-    res.sendStatus(200);
 
     // return 400 for missing field/ invalid auth
 
@@ -119,52 +114,40 @@ app.delete('/reset', async (req, res) => {
 
 })
 
-// Upload endpoint and module ingestion
 // (call logPackageAction) ACTION: CREATE 
 app.post('/package', async (req, res) => {
-    res.send("package endpoint");
 
-    // get time
-    const now = new Date(); // creates a new Date object representing the current date and time
-    const currentTime = now.getTime(); // returns the number of milliseconds since January 1, 1970, 00:00:00 UTC
-
-
-    // get req content
-    const packageContents = req.body["data"]["Contents"];
-    const packageURL = req.body["data"]["URL"];
-    const packageName = req.body["metadata"]["Name"];
-    const packageVersion = req.body["metadata"]["Version"];
-    //const packageID = req.body["metadata"]["ID"];
+    // get req content as PackageData schema
 
     // get auth from header
+
+    // 201
+    // respond with Package schema json object
+
+    // const userName = "Max";
+    // const isAdmin = true;
+    // Extract package metadata from metadata object that needs
+    // to be defined somewhere in this function 
+    // logPackageAction(userName, isAdmin, packageRepo.metaData, "CREATE");
+
+    // 400
+    // malformed json/ invalid auth
+
+    // 403
+    // auth failed (no permissions)
+
+    // 409
+    // package already exists
+
+    // 424
+    // package not uploaded due to disqualification
+
     
-
-    let data = createRepoData(packageName, packageVersion, currentTime.toString(), packageURL, undefined)
-
-    try {
-        // attempt to create and save new package to database
-        const newPackage = await addRepo(data);
-        res.status(201).json(newPackage);
-    } catch (error) {
-        if (error instanceof InvalidRequestError) {
-            res.status(400).send(error.message);
-        } else if (error instanceof AuthenticationError) {
-            res.status(403).send(error.message);
-        } else if (error instanceof PackageAlreadyExistsError) {
-            res.status(409).send(error.message);
-        } else if (error instanceof PackageDisqualificationError) {
-            res.status(424).send(error.message);
-        } else {
-            console.error(error);
-            res.status(500).send("Internal Server Error");
-        }
-    }
 });
 
-// Download Endpoint
 // (call logPackageAction) ACTION: DOWNLOAD
 app.get('/package/:id', async (req, res) => {
-    console.log("package/" + req.params.id + " endpoint");
+    res.send("package/" + req.params.id + " endpoint");
 
     let id = Number(req.params.id);
     const result = await doesIdExistInKind(MODULE_KIND, id)
@@ -172,9 +155,16 @@ app.get('/package/:id', async (req, res) => {
         res.send("req.params.id doesn't exist in MODULE_KIND.");
         return;
     }
-
     // download package by ID
-    res.send(await downloadRepo(id));
+    const packageRepo = await downloadRepo(id);
+    if (packageRepo.ID === id) {
+      //const userName = "Max";
+      //const isAdmin = true;
+      //logPackageAction(userName, isAdmin, packageRepo.metaData, "DOWNLOAD");
+    } else {
+      return res.status(400).json({ message: 'ID MISMATCH' });
+    }
+
     // default response:
     // unexpected error (what error code do we return)
 
@@ -182,46 +172,34 @@ app.get('/package/:id', async (req, res) => {
     // return package schema json object
     //  includes: metadata and data
 
+    // code 400
+    // package not found or bad auth
+
     // code 404
     // package DNE
 });
 
-// Update Endpoint
 // (call logPackageAction), ACTION: UPDATE
 app.put('/package/:id', async (req, res) => {
-    res.send("package/" + req.params.id + " endpoint");
-
-    // get time
-    const now = new Date(); // creates a new Date object representing the current date and time
-    const currentTime = now.getTime(); // returns the number of milliseconds since January 1, 1970, 00:00:00 UTC
-
-
-    // get req content
-    const packageContents = req.body["data"]["Contents"];
-    const packageURL = req.body["data"]["URL"];
-    const packageName = req.body["metadata"]["Name"];
-    const packageVersion = req.body["metadata"]["Version"];
-    const packageID = req.body["metadata"]["ID"];
-
-    // get auth from header
-    
-
-    let data = createRepoData(packageName, packageVersion, currentTime.toString(), packageURL, undefined)
-
-    try {
-        // attempt to create and save new package to database
-        const newPackage = await updateRepo(packageID, data);
-        res.status(200).json(newPackage);
-    } catch (error) {
-        if (error instanceof InvalidRequestError) {
-            res.status(400).send(error.message);
-        } else if (error instanceof PackageDoesNotExist) {
-            res.status(404).send(error.message);
-        } else {
-            console.error(error);
-            res.status(500).send("Internal Server Error");
-        }
+    let id = Number(req.params.id);
+    const result = await doesIdExistInKind(MODULE_KIND, id)
+    if(!result){
+        res.send("req.params.id doesn't exist in MODULE_KIND.");
+        return;
     }
+    // download package by ID
+    const packageRepo = await downloadRepo(id);
+    if (packageRepo.ID === id) {
+      //const userName = "Max";
+      //const isAdmin = true;
+      //logPackageAction(userName, isAdmin, packageRepo.metaData, "UPDATE");
+    } else {
+      return res.status(400).json({ message: 'ID MISMATCH' });
+    }
+
+
+
+
 
     // get package schema from request body
 
@@ -239,7 +217,6 @@ app.put('/package/:id', async (req, res) => {
 
 });
 
-// Delete endpoint
 app.delete('/package/:id', async (req, res) => {
     res.send("package/" + req.params.id + " endpoint");
 
@@ -255,24 +232,96 @@ app.delete('/package/:id', async (req, res) => {
     // package DNE
 });
 
-// Rate endpoint
 // (call logPackageAction), ACTION: RATE
-app.get('/package/:id/rate', (req, res) => {
-    res.send("package/" + req.params.id + "/rate endpoint");
+app.get('/package/:id/rate', async (req, res) => {
+    // Extract package ID and authentication token from request params and headers
+    const packageID = Number(req.params.id);
 
-    // get req with PackageID and AuthenticationToken schema
-    // respond with content as PackageRating schema
+    const result = await doesIdExistInKind(MODULE_KIND, packageID)
+    if(!result){
+        // 404: Package does not exist.
+        res.status(404).send({error: 'Package does not exist'});
+        return;
+    }
+    // Download the package entity
+    const packageRepo = await downloadRepo(packageID);
+    //const userName = "Max";
+    //const isAdmin = true;
+    //logPackageAction(userName, isAdmin, packageRepo.metaData, "RATE");
 
-    // 400
-    // malformed json/ invalid auth
+    // Needed for Rate
+    console.log(packageRepo.url);
+    const url = packageRepo.url;
 
-    // 404
-    // package DNE
+    // Write the url to a file called URLs.txt
+    fs.writeFileSync('URLs.txt', url);
 
-    // 500
-    // package choked on one metric
+    // Define the type signature of the Rust function
+    const handle_url_file = ffi.Library('./target/release/libmylib', {
+      'handle_url_file': ['void', ['string', 'string', 'int']]
+    }).handle_url_file;
+
+    // Call the Rust function and output the result to the console
+    handle_url_file("URLs.txt", "example.log", 1);
+    
+    console.log("Success!!!")
+
+    // Read the contents of the metrics.txt file
+    const metrics = fs.readFileSync('metrics.txt', 'utf-8');
+
+    try {
+      // Parse the JSON string into a JavaScript object
+      const metricsObject = JSON.parse(metrics);
+      
+      // Extract the properties and convert the values to their numeric form
+      const url1 = metricsObject.URL;
+      const netScore = parseFloat(metricsObject.NET_SCORE);
+      const rampUp = parseFloat(metricsObject.RAMP_UP_SCORE);
+      const correctness = parseFloat(metricsObject.CORRECTNESS_SCORE);
+      const busFactor = parseFloat(metricsObject.BUS_FACTOR_SCORE);
+      const responsiveMaintainer = parseFloat(metricsObject.RESPONSIVE_MAINTAINER_SCORE);
+      const license = parseFloat(metricsObject.LICENSE_SCORE);
+      const codeReview = parseFloat(metricsObject.CODE_REVIEW);
+      const version = parseFloat(metricsObject.Version_Pinning);
+    
+      // Construct the response object
+      const responseObject = {
+        BusFactor: busFactor,
+        Correctness: correctness,
+        RampUp: rampUp,
+        ResponsiveMaintainer: responsiveMaintainer,
+        LicenseScore: license,
+        GoodPinningPractice: version,
+        PullRequest: codeReview,
+        NetScore: netScore
+      };
+      
+      // 200: Only send a 200 response if each metric was computed successfully
+      if (true) {
+        // Send the response object to the client
+        res.status(200).send(responseObject);
+      } else {
+        // 500: The package rating system choked on at least one of the metrics.
+        res.status(500).send({error: 'The package rating system choked on at least one of the metrics'});
+      }
+    } catch (error) {
+      // If there was an error parsing the JSON string
+      res.status(400).send({ error: 'Malformed json' });
+    }
 });
 
+// Checks if the package name adheres to the naming conventions
+function nameConv(name: string): boolean {
+  for (let i = 0; i < name.length; i++) {
+    const charCode = name.charCodeAt(i);
+    // every character one can type on the keyboard
+    // ASCII codes 32 (space) to 126 (~).
+    if (charCode < 32 || charCode > 126) {
+      return false;
+    }
+  }
+  return true;
+}
 // Return the history of this package (all versions).
 app.get('/package/byName/:name', async (req, res) => {
     try {
@@ -284,8 +333,7 @@ app.get('/package/byName/:name', async (req, res) => {
       // - The name "*" is reserved. See the `/packages` API for its meaning.
       
       // Check if the package name adheres to the naming conventions
-      const filter = /^[a-zA-Z0-9\-._~!$&'()*+,;=]+$/.test(packageName);
-      if (!filter || packageName === '*') {
+      if (!nameConv(packageName) || packageName === '*') {
         // 400 - invalid package name
         res.status(400).json({error: 'Invalid package name'});
       } else {
@@ -311,62 +359,51 @@ app.get('/package/byName/:name', async (req, res) => {
     }
 });
 
-
-/**
- * Logs a package action in the repository for the given user and package.
- *
- * @param {string} userName - The name of the user performing the action.
- * @param {boolean} isAdmin - Indicates if the user is an administrator.
- * 
- * @param {string} packageName - The name of the package being acted upon.
- * @param {string} packageVersion - The version of the package being acted upon.
- * The "packageName" and "packageVersion" are used as a unique identifier pair when uploading a package.
- * 
- * @param {string} packageID - The unique identifier of the package being acted upon.
- *   This is used as an internal identifier for interacting with existing packages.
- *   packageID is used with the /package/{id} endpoint.
- * 
- * @param {string} action - The action being performed on the package [ CREATE, UPDATE, DOWNLOAD, RATE ].
- */
-
-// Follows the structure of the PackageHistoryEntry Schema
-async function logPackageAction(userName: string, isAdmin: boolean, packageName: string, packageVersion: string, packageID: string, action: string) {
-    const now = new Date();
-    const packageAction = {
-      User: {
-        name: userName,
-        isAdmin: isAdmin
-      },
-      Date: now,
-      PackageMetadata: {
-        Name: packageName,
-        Version: packageVersion,
-        ID: packageID
-      },
-      Action: action
-    };
-    // Updates the packageAction field of a package in the datastore for the given repository ID.
-    await updateRepoPackageAction(packageID, packageAction);
-}
-
-
-// Delete endpoint
+// Deletes all versions of a package from the datastore with the given name.
 app.delete('/package/byName/:name', async (req, res) => {
-
     // get package name from header
-    // get auth token from header
+    const packageName = req.params.name;
+    
+    // Check if the package name adheres to the naming conventions
+    if (!nameConv(packageName) || packageName === '*') {
+        // 400 - invalid package name
+        res.status(400).json({error: 'Invalid package name'});
+    } else {
+        // Retrieve all packages from the datastore with that package name
+        const allPackages = await findReposByName(packageName);
 
-    // 200
-    // package successfully deleted
+        if (allPackages.length === 0) {
+            // 404 - package does not exist
+            res.status(404).json({error: 'Package does not exist'});
+        } else {
 
-    // 400
-    // malformed json/ invalid auth
+            let id = null;
+            const deletionPromises = [];
 
-    // 404
-    // package DNE
+            // Iterates over each package
+            for (const pkg of allPackages) {
+            const symbolKeys = Object.getOwnPropertySymbols(pkg);
+            // Iterates over each key in dictionary
+            for (const symbolKey of symbolKeys) {
+                if (symbolKey.toString() === 'Symbol(KEY)') {
+                // Extracts the ID
+                id = pkg[symbolKey].id;
+                console.log(id)
+                break;
+                }
+            }
+            deletionPromises.push(deleteRepo(Number(id)));
+            }
+            // Delete all versions of the package from the datastore
+            await Promise.all(deletionPromises);
+            // 200 - package successfully deleted
+            res.status(200).json({ message: `All versions of package ${packageName} have been deleted` });
+        }
+    }
 });
 
-// Fetch package with Regex
+
+// Get any packages fitting the regular expression
 app.post('/package/byRegEx', async (req, res) => {
     // Check if the request has a JSON body
     if (Object.keys(req.body).length === 0) {
@@ -407,9 +444,7 @@ app.post('/package/byRegEx', async (req, res) => {
     }
 });
 
-// Username-password authentication
 app.put('/authenticate', (req, res) => {
-    res.send("authenticate endpoint");
 
     // get AuthenticationRequest schema
 
@@ -423,10 +458,47 @@ app.put('/authenticate', (req, res) => {
     // username or password invalid
 
     // 501
-    // this system does not support authentication
-
+    // This system does not support authentication
+    return res.status(501).json({ message: 'This system does not support authentication' });
 });
 
+/**
+ * Logs a package action in the repository for the given user and package.
+ *
+ * @param {string} userName - The name of the user performing the action.
+ * @param {boolean} isAdmin - Indicates if the user is an administrator.
+ * 
+ * @param {string} packageName - The name of the package being acted upon.
+ * @param {string} packageVersion - The version of the package being acted upon.
+ * The "packageName" and "packageVersion" are used as a unique identifier pair when uploading a package.
+ * 
+ * @param {string} packageID - The unique identifier of the package being acted upon.
+ *   This is used as an internal identifier for interacting with existing packages.
+ *   packageID is used with the /package/{id} endpoint.
+ * 
+ * @param {string} action - The action being performed on the package [ CREATE, UPDATE, DOWNLOAD, RATE ].
+ */
+
+// Follows the structure of the PackageHistoryEntry Schema
+async function logPackageAction(userName: string, isAdmin: boolean, packageRepo: any, action: string) {
+    const now = new Date();
+    const packageAction = {
+      User: {
+        name: userName,
+        isAdmin: isAdmin
+      },
+      Date: now,
+      PackageMetadata: {
+        Name: packageRepo.Name,
+        Version: packageRepo.Version,
+        ID: packageRepo.ID
+      },
+      Action: action
+    };
+
+    // Updates the packageAction field of a package in the datastore for the given repository ID.
+    await updateRepoPackageAction(packageRepo.ID, packageAction);
+}
 
 /* * * * * * * * * * * * * * *
  * Website Serving endpoints *
@@ -442,8 +514,12 @@ app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, HTML_PATH + "/index.html"));
     res.send("index!");
 
+    //createRepoData("max-repooooo", "1.43.34", new Date().toJSON(), "perl.gob", "perl.gob", [], metaData);
+
+    
+
 });
 
 app.listen(port, () => {
-    console.log("The application is listening on port " + port + "!");
+    console.log(`The application is listening on port ${port}!`);
 });
