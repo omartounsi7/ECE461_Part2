55a7c722091cd00ffb056032ac9a2252ac2874c3
kevintl1998 <kevintl1998@gmail.com>
2023-04-28 23:12:35 -0400

    added secret key fetching

--- ts-server/src/cloud-storage/cs_config.ts
+++ ts-server/src/cloud-storage/cs_config.ts
@@ -4,8 +4,10 @@ const storage = new Storage({
     // projectId: "npm-module-registry-381816"
 });
 
+const SECRET_STORAGE_BUCKET = "ece461-secrets";
+
 const LOG_STORAGE_BUCKET = "ece461-logs";
 
 const MODULE_STORAGE_BUCKET = "ece461-repositories";
 
-export { storage, MODULE_STORAGE_BUCKET, LOG_STORAGE_BUCKET };
+export { storage, MODULE_STORAGE_BUCKET, LOG_STORAGE_BUCKET, SECRET_STORAGE_BUCKET };
